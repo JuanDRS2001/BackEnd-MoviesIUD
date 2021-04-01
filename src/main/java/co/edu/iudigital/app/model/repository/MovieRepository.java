6bc5ab98f15eb67161802391ef5376f1344ee216
Juan David <juandavidr.s@hotmail.com>
2021-03-31 20:34:40 -0500

    1r Commit BackEnd

---
+++ src/main/java/co/edu/iudigital/app/model/repository/MovieRepository.java
@@ -0,0 +1,7 @@
+package co.edu.iudigital.app.model.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import co.edu.iudigital.app.model.entity.Movie;
+
+public interface MovieRepository extends CrudRepository<Movie, Integer>{
+}
---
+++ src/main/java/co/edu/iudigital/app/model/repository/UsersRepository.java
@@ -0,0 +1,8 @@
+package co.edu.iudigital.app.model.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import co.edu.iudigital.app.model.entity.Users;
+
+public interface UsersRepository extends CrudRepository<Users, Integer>{
+
+}
